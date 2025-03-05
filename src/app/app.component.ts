d164ed3f413418e6bebd2a96da497e7a948543bc
ankitjain <me.ankit88@gmail.com>
2025-03-04 23:20:38 -0500

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ButtonModule} from 'primeng/button';
+import {Checkbox} from 'primeng/checkbox';
+import {FormsModule} from '@angular/forms';
+import {Image} from 'primeng/image';
+import { GalleriaModule } from 'primeng/galleria';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonModule, Checkbox, FormsModule, GalleriaModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true
 })
 export class AppComponent {
   title = 'angJavaJenkins';
+  checked: any;
+  pizza: any;
+  images = '';
+
+  prev() {
+
+  }
+
+  next() {
+
+  }
 }
