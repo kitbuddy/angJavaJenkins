a01c69f756ed166d7dcafaa3732c40a967c43eb1
ankitjain <me.ankit88@gmail.com>
2025-03-10 21:46:19 -0400

    Added basic component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,16 @@
 import { Component } from '@angular/core';
 import {ButtonModule} from 'primeng/button';
-import {Checkbox} from 'primeng/checkbox';
 import {FormsModule} from '@angular/forms';
 import { GalleriaModule } from 'primeng/galleria';
+import {BasicComponentComponent} from './basic-component/basic-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonModule, Checkbox, FormsModule, GalleriaModule],
+  imports: [ButtonModule, FormsModule, GalleriaModule, BasicComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true
 })
 export class AppComponent {
-  title = 'angJavaJenkins';
-  checked: any;
-  pizza: any;
-  images = '';
 
-  prev() {
-
-  }
-
-  next() {
-
-  }
 }
