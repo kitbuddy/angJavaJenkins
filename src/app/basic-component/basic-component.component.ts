767a947e1f4185398e68d3819d1e46cfbf627346
ankitjain <me.ankit88@gmail.com>
2025-03-10 21:46:35 -0400

    Added basic component

---
+++ src/app/basic-component/basic-component.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {Button} from "primeng/button";
+import {Checkbox} from "primeng/checkbox";
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-basic-component',
+  imports: [
+    Button,
+    Checkbox,
+    FormsModule
+  ],
+  templateUrl: './basic-component.component.html',
+  styleUrl: './basic-component.component.scss'
+})
+export class BasicComponentComponent {
+  pizza: any;
+}
